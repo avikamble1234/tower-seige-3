601edca78fd577b7349e318ed048627812d6dc79
avikamble1234 <78914844+avikamble1234@users.noreply.github.com>
2021-04-16 21:58:25 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,148 +1,243 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var stand1, stand1, ground1;
-var box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11, box12, box13, box14, box15, box16, box17, box18;
-var polygon1, slingShot;
+var ground;
+var stand1, stand2;
+var box1, box2, box3, box4, box5, box6, box7, box8, box9, box10;
+var box11, box12, box13, box14, box15, box16;
+var sbox1, sbox2, sbox3, sbox4, sbox5, sbox6, sbox7, sbox8, sbox9;
+var hex, heximg;
+var launch;
+var bg = 0;
+var backgroundImg; 
 var score = 0;
-var backgroundImg;
-var gameState = "on slingshot";
-var bg = "Sprites/bg.jpg";
 
-function preload() {
-  getBackgroundImg();
-  backgroundImg = loadImage("Sprites/bg.jpg");
+function preload()
+{
+    heximg = loadImage("polygon.png");
+    backgroundImg = bg;
+    getTime();
+    
+    
 }
 
-function setup() {
-  createCanvas(800,400);
-  engine = Engine.create();
-  world = engine.world;
 
-  createSprite(400, 200, 50, 50);
+function setup()
+{
+    createCanvas(1600,800);
 
-  stand1 = new Ground(390,height - 100,200,20);
-  stand2 = new Ground(650,height - 200,200,20);
-  ground1 = new Ground(400,height + 50,800,20);
+    engine = Engine.create();
+    world = engine.world;
 
-  box1 = new Box(330, 235, 30, 40);
-  box2 = new Box(360, 235, 30, 40);
-  box3 = new Box(390, 235, 30, 40);
-  box4 = new Box(420, 235, 30, 40);
-  box5 = new Box(450, 235, 30, 40);
+    ground = new Ground(800,795,1600,10);
+    stand1 = new Stand(700,650,300,10);
+    stand2 = new Stand(1200,400,225,10);
 
-  box6 = new Box(360, 195, 30, 40);
-  box7 = new Box(390, 195, 30, 40);
-  box8 = new Box(420, 195, 30, 40);
+    var options = {
+        'restitution':0.8,
+        'friction':1.0,
+        'density':1.0
+    }
+    hex = Bodies.circle(150, 600, 20, options);
+    World.add(world, hex);
 
-  box9 = new Box(390, 185, 30, 40);
+    
 
-  box10 = new Box(590, 135, 30, 40);
-  box11 = new Box(620, 135, 30, 40);
-  box12 = new Box(650, 135, 30, 40);
-  box13 = new Box(680, 135, 30, 40);
-  box14 = new Box(710, 135, 30, 40);
+    launch = new Launcher(this.hex,{x:150, y:600});
 
-  box15 = new Box(620, 85, 30, 40);
-  box16 = new Box(650, 85, 30, 40);
-  box17 = new Box(680, 85, 30, 40);
+    //level 1
+    box1 = new Box(580, 620, 40, 60);
+    box2 = new Box(620, 620, 40, 60);
+    box3 = new Box(660, 620, 40, 60);
+    box4 = new Box(700, 620, 40, 60);
+    box5 = new Box(740, 620, 40, 60);
+    box6 = new Box(780, 620, 40, 60);
+    box7 = new Box(820, 620, 40, 60);
 
-  box18 = new Box(650, 55, 30, 40);
+    //level 2
+    box8 = new Box(620, 560, 40, 60);
+    box9 = new Box(660, 560, 40, 60);
+    box10 = new Box(700, 560, 40, 60);
+    box11 = new Box(740, 560, 40, 60);
+    box12 = new Box(780, 560, 40, 60);
 
-  polygon1 = new Polygon(100, 280, 40);
+    //level 3
+    box13 = new Box(660, 500, 40, 60);
+    box14 = new Box(700, 500, 40, 60);
+    box15 = new Box(740, 500, 40, 60);
 
-  slingShot = new SlingShot(polygon1.body, {x: 150, y: 150});
+    //level 4
+    box16 = new Box(700, 440, 40, 60);
+
+    //level 1
+    sbox1 = new Box(1120, 360, 40, 60);
+    sbox2 = new Box(1160, 360, 40, 60);
+    sbox3 = new Box(1200, 360, 40, 60);
+    sbox4 = new Box(1240, 360, 40, 60);
+    sbox5 = new Box(1280, 360, 40, 60);
+
+    //level 2
+    sbox6 = new Box(1160, 300, 40, 60);
+    sbox7 = new Box(1200, 300, 40, 60);
+    sbox8 = new Box(1240, 300, 40, 60);
+
+    //level 3
+    sbox9 = new Box(1200, 240, 40, 60);
+
+    Engine.run(engine);
+    //console.log(box1.body.position.y);
 }
 
-function draw() {
-  if(backgroundImg) {
+function draw()
+{   
+    if(background(backgroundImg))
     background(backgroundImg);
+
+
+    noStroke();
+    textSize(35);
+    fill("blue");
+    text("Score : "+ score, width-300, 50);
+
+    ground.display();
+    stand1.display();
+    stand2.display();
+
+    imageMode(CENTER);
+    image(heximg, hex.position.x, hex.position.y, 50, 50);
+
+    //level 1
+    stroke(0);
+    strokeWeight(2);
+    fill("lightblue");
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    box6.display();
+    box7.display();
+
+    box1.score();
+    box2.score();
+    box3.score();
+    box4.score();
+    box5.score();
+    box6.score();
+    box7.score();
+
+
+    //level 2
+    stroke(0);
+    strokeWeight(2);
+    fill("pink");
+    box8.display();
+    box9.display();
+    box10.display();
+    box11.display();
+    box12.display();
+
+    box8.score();
+    box9.score();
+    box10.score();
+    box11.score();
+    box12.score();
+    //level 3
+    stroke(0);
+    strokeWeight(2);
+    fill("lightgreen");
+    box13.display();
+    box14.display();
+    box15.display();
+
+    box13.score()
+    box14.score()
+    box15.score()
+    
+    stroke(0);
+    strokeWeight(2);
+    fill("lightyellow");
+    box16.display();
+    box16.score();
+
+    //level 1
+    stroke(0);
+    strokeWeight(2);
+    fill("lightblue");
+    sbox1.display();
+    sbox2.display();
+    sbox3.display();
+    sbox4.display();
+    sbox5.display();
+
+    sbox1.score();
+    sbox2.score();
+    sbox3.score();
+    sbox4.score();
+    sbox5.score();
+
+
+    //level 2
+    stroke(0);
+    strokeWeight(2);
+    fill("pink");
+    sbox6.display();
+    sbox7.display();
+    sbox8.display();
+
+    sbox6.score();
+    sbox7.score();
+    sbox8.score();
+
+
+    //level 3
+    stroke(0);
+    strokeWeight(2);
+    fill("lightgreen");
+    sbox9.display();
+
+    sbox9.score();
 }
-  
-  noStroke();
-  textSize(20);
-  fill("white");
-  text("SCORE :" + score, 650, 40);
-  text("Press the space key for another chance!", 400, 380);
 
-  Engine.update(engine);
 
-  stroke("black");
-
-  box1.score();
-  box1.display();
-  box2.score();
-  box2.display();
-  box3.score();
-  box3.display();
-  box4.score();
-  box4.display();
-  box5.score();
-  box5.display();
-  box6.score();
-  box6.display();
-  box7.score();
-  box7.display();
-  box8.score();
-  box8.display();
-  box9.score();
-  box9.display();
-  box10.score();
-  box10.display();
-  box11.score();
-  box11.display();
-  box12.score();
-  box12.display();
-  box13.score();
-  box13.display();
-  box14.score();
-  box14.display();
-  box15.score();
-  box15.display();
-  box16.score();
-  box16.display();
-  box17.score();
-  box17.display();
-  box18.score();
-  box18.display();
-  stand1.display();
-  stand2.display()
-  ground1.display();
-  polygon1.display();
-  slingShot.display();
+function mouseDragged()
+{
+	Matter.Body.setPosition(this.hex,{x: mouseX, y: mouseY})
 }
 
-function mouseDragged(){
-  if(gameState!=="launched"){
-    Matter.Body.setPosition(polygon1.body, {x: mouseX , y: mouseY});
-  }
+function mouseReleased()
+{
+	launch.fly();
 }
 
-function mouseReleased() {
-  slingShot.fly();
-  gameState = "launched";
+function keyPressed()
+{
+    if(keyCode === 32)
+    {
+        launch.attach(this.hex);
+    }
 }
 
-function keyPressed() {
-  if(keyCode === 32) {
-      slingShot.attach(polygon1.body);
-      gameState = "on slingshot";
-  }
-}
+async function getTime(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var responseJSON = await response.json();
 
-async function getBackgroundImg() {
-  var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
-  var responseJSON = await response.json();
-  var date = responseJSON.datetime;
-  var hour = date.slice(11, 13);
-  if(hour>=06 && hour<=19){
-      bg = "Sprites/bg.jpg"
-  } else {
-      bg = "Sprites/bg1.jpg"
-  }
-  backgroundImg = loadImage(bg);
-  console.log(backgroundImg);
+    var daytime = responseJSON.datetime;
+    var hour = daytime.slice(11, 13);
+    console.log(hour);
+
+    if(hour >= 6 && hour < 19)
+    {
+       bg = "lightblue";
+    }
+    else{
+        bg = "black" ;
+       
+    }
+
+    backgroundImg = bg;
 }
